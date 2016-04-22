f0f06dd23af883f15682d09412903db593cd8211
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-22 11:55:09 -0700

    finished GET and POST routes

--- routes/router.js
+++ routes/router.js
@@ -4,13 +4,35 @@ const Motor = require(__dirname + '/../models/motor');
 const bodyParser = require('body-parser').json();
 const myRouter = module.exports = new Router();
 
-myRouter.get('/pedal');
+myRouter.get('/pedal', (req, res) => {
+  Pedal.find(null, (err, data) => {
+    if (err) return res.status(500).send('Server Error');
+    res.status(200).json(data);
+  });
+});
 
-myRouter.get('/motor');
+myRouter.get('/motor', (req, res) => {
+  Motor.find(null, (err, data) => {
+    if (err) return res.status(500).send('Server Error');
+    res.status(200).json(data);
+  });
+});
 
-myRouter.post('/pedal');
+myRouter.post('/pedal', bodyParser, (req, res) => {
+  var newPedal = new Pedal(req.body);
+  newPedal.save((err, data) => {
+    if (err) return res.status(500).send('Server Error');
+    res.status(200).json(data);
+  });
+});
 
-myRouter.post('/motor');
+myRouter.post('/motor', bodyParser, (req, res) => {
+  var newMotor = new Motor(req.body);
+  newMotor.save((err. data) => {
+    if (err) return res.status(500).send('Server Error');
+    res.status(200).json(data);
+  })
+});
 
 myRouter.put('/pedal');
 
